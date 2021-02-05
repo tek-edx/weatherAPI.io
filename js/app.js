df6383cbe7e978a46b389b6a126434bb73b185be
Tek <edx.tek@gmail.com>
2021-02-05 12:01:22 +1100

    Add user interface to enter city name and display weather

---
+++ js/app.js
@@ -0,0 +1,49 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector(".details");
+
+const updateUI = (data) => {
+
+    const { cityDetails, weather } = data;
+
+
+
+    //update details templates 
+
+    details.innerHTML = `
+     <h5 class="my-3">${cityDetails.EnglishName}</h5>
+          <div class="my-3">${weather.WeatherText}</div>
+          <div class="display-4 my-4">
+              <span>${weather.Temperature.Metric.Value}</span>
+              <span>&deg;C</span>
+          </div>`
+
+    //remove the d-none class if present
+
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none')
+    }
+};
+
+const updateCity = async (city) => {
+
+    const cityDetails = await getCity(city);
+    const weather = await getWeather(cityDetails.Key);
+
+    return {
+        cityDetails,
+        weather
+    }
+
+}
+
+cityForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // get city value 
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+    // update he ui with new city 
+    updateCity(city).then(data => {
+        updateUI(data)
+    }).catch(err => console.log(err));
+});
