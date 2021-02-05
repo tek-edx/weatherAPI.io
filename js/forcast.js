4789f49b7dfb976700f49b77ca6f2432f58434d0
Tek <edx.tek@gmail.com>
2021-02-05 18:02:18 +1100

    Change http to https

--- js/forcast.js
+++ js/forcast.js
@@ -3,7 +3,7 @@ const key = "LCtNeAPQWtE5pNnrxnY5iMzwpvRqJGne";
 
 const getWeather = async (id) => {
 
-    const base = "https://dataservice.accuweather.com/currentconditions/v1/";
+    const base = "httpss://dataservice.accuweather.com/currentconditions/v1/";
     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base + query);
@@ -18,7 +18,7 @@ const getWeather = async (id) => {
 
 const getCity = async (city) => {
 
-    const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
+    const base = "httpss://dataservice.accuweather.com/locations/v1/cities/search";
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
